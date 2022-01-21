df6a516e0f9e9f371fa07049228eb61c323301d4
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2022-01-22 00:19:51 +0100

    search view

--- lide_api/views.py
+++ lide_api/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, HttpRequest
 from django.shortcuts import get_object_or_404
 from django.views import View
 from django.core.paginator import Paginator, Page
+from django.db.models import Q
 from .models import Offers
 
 
@@ -21,8 +22,8 @@ class OffersView(View):
         offers:Dict[str, Any] = {
             "results": self.get_current_page(current_page),
             "pages" : paginator.num_pages,
-            "current_page": current_page.number,
-            "page_range": list(paginator.get_elided_page_range(current_page.number)) 
+            "currentPage": current_page.number,
+            "pageRange": list(paginator.get_elided_page_range(current_page.number)) 
         }
         return HttpResponse(json.dumps(offers, default=str, indent=4),
                             content_type='application/json', )
@@ -56,3 +57,39 @@ class OfferDetails(View):
         return HttpResponse(json.dumps(result, default=str, indent=4),
                             content_type='application/json', )
         
+class OfferSearch(View):
+    def get(self, request: HttpRequest):
+        try:
+            page_no:int = int(request.GET.get("page", "1"))
+        except ValueError:
+            page_no: int = 1
+
+        query:str = str(request.GET.get("q", ""))
+
+        offer_list: List[Dict[Any, Any]] = list(Offers.objects.all().filter(posted__exact=True)
+                                                                    .order_by("-edited")
+                                                                    .filter(Q(position___name__icontains=query) | Q(location___name__icontains=query) | Q(employment_type___name__icontains=query) | Q(details__icontains=query)))
+        paginator = Paginator(offer_list, per_page=2, allow_empty_first_page=True)
+        current_page: Page = paginator.get_page(page_no)
+        offers:Dict[str, Any] = {
+            "results": self.get_current_page(current_page),
+            "pages" : paginator.num_pages,
+            "currentPage": current_page.number,
+            "pageRange": list(paginator.get_elided_page_range(current_page.number)) 
+        }
+        return HttpResponse(json.dumps(offers, default=str, indent=4),
+                            content_type='application/json', )
+                            # headers = {'Access-Control-Allow-Origin':"*"})
+        
+
+    def get_current_page(self, current_page: Page) -> List[Dict[str, Any]]:
+        result = []
+        for offer in current_page.object_list:
+            result.append({
+                "id": offer.pk,
+                "position": offer.position,
+                "location": list(offer.location.all()),
+                "employmentType": list(offer.employment_type.all()),
+                "edited": offer.edited
+            }) 
+        return result
