be154ba291d7bdb54d1f14f52c70319b786eacd6
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2022-02-24 17:22:42 +0100

    static

--- lide/settings.py
+++ lide/settings.py
@@ -122,7 +122,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/lide/static/"
 
 MEDIA_ROOT = "/home/seba/workspace/lide_backend/lide/media"
 
